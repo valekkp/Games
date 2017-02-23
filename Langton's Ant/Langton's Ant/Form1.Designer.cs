657ae01ccf2b9c470405ea82faa1a55981b1bed0
valekkp <valekkp@rambler.ru>
2017-02-23 16:16:38 +0300

    Langton's Ant.

---
+++ Langton's Ant/Langton's Ant/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Langton_s_Ant
+{
+    partial class GamingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GamingBoard = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.GamingBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GamingBoard
+            // 
+            this.GamingBoard.Location = new System.Drawing.Point(0, 0);
+            this.GamingBoard.Name = "GamingBoard";
+            this.GamingBoard.Size = new System.Drawing.Size(272, 249);
+            this.GamingBoard.TabIndex = 0;
+            this.GamingBoard.TabStop = false;
+            this.GamingBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.GamingBoard_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // GamingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.GamingBoard);
+            this.DoubleBuffered = true;
+            this.Name = "GamingForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.GamingBoard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox GamingBoard;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Langton's Ant/Langton's Ant/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Langton_s_Ant
+{
+    public partial class GamingForm : Form
+    {
+        private const int CellSize = 10;
+
+        Color[,] brushes = new Color[600 / CellSize, 800 / CellSize];
+
+        Point ant = new Point(300 / CellSize, 400 / CellSize);
+
+        enum Direction
+        {
+            Up,
+            Right,
+            Down,
+            Left
+        }
+
+        enum Color
+        {
+            White,
+            Red,
+            Green,
+            Blue
+        }
+
+        private Direction direction = Direction.Up;
+
+        private Dictionary<Brush, string> rules = new Dictionary<Brush, string>()
+        {
+            { Brushes.White, "left" },
+            { Brushes.Red, "right"}
+        };
+
+        public GamingForm()
+        {
+            InitializeComponent();
+            GamingBoard.Size = new Size(800, 600);
+            GamingBoard.Location = new Point(0, 0);
+            Size = new Size(850, 650);
+            Start();
+        }
+
+        public void Start()
+        {
+            timer1.Interval = 1;
+            timer1.Start();
+            for (int i = 0; i < 600 / CellSize; i++)
+            {
+                for (int j = 0; j < 800 / CellSize; j++)
+                {
+                    brushes[i,j] = Color.White;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            switch (brushes[ant.Y, ant.X])
+            {
+                case Color.White:
+                    direction = (Direction) Math.Abs(((int) direction + 1) % 4);
+                    break;
+                case Color.Red:
+                    direction = (Direction) (((int)direction - 1 > -1 ? (int)direction - 1 : 3) % 4);
+                    break;
+                case Color.Green:
+                    direction = (Direction)Math.Abs(((int)direction + 1) % 4);
+                    break;
+                case Color.Blue:
+                    direction = (Direction)(((int)direction - 1 > -1 ? (int)direction - 1 : 3) % 4);
+                    break;
+            }
+
+            brushes[ant.Y, ant.X] = (Color) ((int) (brushes[ant.Y, ant.X] + 1) % 4);
+
+            GamingBoard.Refresh();
+
+            GamingBoard.Update();
+            switch (direction)
+            {
+                case Direction.Up:
+                    ant.Y--;
+                    break;
+                case Direction.Right:
+                    ant.X++;
+                    break;
+                case Direction.Down:
+                    ant.Y++;
+                    break;
+                case Direction.Left:
+                    ant.X--;
+                    break;
+            }
+        }
+
+        private void GamingBoard_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < 600 / CellSize; i++)
+            {
+                for (int j = 0; j < 800 / CellSize; j++)
+                {
+                    Brush brush = Brushes.White;
+                    switch (brushes[i, j])
+                    {
+                        case Color.White:
+                            brush = Brushes.White;
+                            break;
+                        case Color.Red:
+                            brush = Brushes.Red;
+                            break;
+                        case Color.Green:
+                            brush = Brushes.Green;
+                            break;
+                        case Color.Blue:
+                            brush = Brushes.Blue;
+                            break;
+                    }
+
+                    e.Graphics.FillRectangle(brush, i * CellSize, j * CellSize, CellSize, CellSize);
+                }
+            }
+        }
+    }
+}
